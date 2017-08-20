76056be1f2a441253c4d6a18f2b3439eee4addb0
djo <declan.l.johnson@gmail.com>
2017-08-20 04:27:06 -0500

    Add solution and solution fingerprint

--- worker.py
+++ worker.py
@@ -2,20 +2,7 @@ import numpy as np
 import hashlib
 from scipy import signal
 
-class Job:
-
-    id = 0
-    data1 = 0
-    data2 = 0
-    op = 0
-    solution = 0
-    solution_fingerprint = 0
-
-    def __init__(self, data1, data2, op):
-        self.data1 = data1
-        self.data2 = data2
-        self.dop = op
-        id = keccak256(data1, data2, op)
+WORKER = "0xd3adb33f"
 
 def readFile(name):
     with open(name) as f:
@@ -31,6 +18,31 @@ def keccak256(*args):
     for arg in args:
         al.update(arg.encode('utf-8'))
     return al.hexdigest()
+
+class Job:
+
+    id = 0
+    data1 = 0
+    data2 = 0
+    op = 0
+    solution = 0
+    solver = 0
+    solution_fingerprint = 0
+
+    def __init__(self, data1, data2, op):
+        self.data1 = data1
+        self.data2 = data2
+        self.dop = op
+        id = keccak256(data1, data2, op)
+
+    
+    def solve(self, solver, solution):
+        self.solution = solution
+        self.solver = solver
+        self.solution_fingerprint = keccak256(id, solver, solution)
+
+
+
 
 mat1 = readFile('data1')
 mat2 = readFile('data2')
