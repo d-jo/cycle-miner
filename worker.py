724f28484b723736ea94e81377811b1a6a939e2e
djo <declan.l.johnson@gmail.com>
2017-08-19 22:21:36 -0500

    Add sha3 keccak

--- worker.py
+++ worker.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sha3
 from scipy import signal
 
 def readFile(name):
@@ -10,6 +11,10 @@ def readFile(name):
 def conv2d(mat1, mat2, padding='valid'):
     return signal.convolve2d(mat1, mat2, padding)
 
+def keccak256(data1, data2, op):
+    al = sha3.keccak_256()
+    al.update(data1, data2, op)
+    return al.hexdigest()
 
 mat1 = readFile('data1')
 mat2 = readFile('data2')
