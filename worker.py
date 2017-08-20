f2c3187443bf65543e6b3993c4f5de4806fea388
djo <declan.l.johnson@gmail.com>
2017-08-20 03:36:54 -0500

    Add sha3 keccak356 method

--- worker.py
+++ worker.py
@@ -1,5 +1,5 @@
 import numpy as np
-import sha3
+import hashlib
 from scipy import signal
 
 def readFile(name):
@@ -12,13 +12,16 @@ def conv2d(mat1, mat2, padding='valid'):
     return signal.convolve2d(mat1, mat2, padding)
 
 def keccak256(data1, data2, op):
-    al = sha3.keccak_256()
-    al.update(data1, data2, op)
+    al = hashlib.sha256()
+    al.update(data1.encode('utf-8'))
+    al.update(data2.encode('utf-8'))
+    al.update(op.encode('utf-8'))
     return al.hexdigest()
 
 mat1 = readFile('data1')
 mat2 = readFile('data2')
 
+print(keccak256(mat1, mat2, "conv"))
 print(mat1)
 print(mat2)
 
