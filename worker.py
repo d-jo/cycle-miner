7d5d07234fa95745ff26b3ea8b829ca9a08f271a
djo <declan.l.johnson@gmail.com>
2017-08-25 16:29:33 -0500

    Add helper method for creating contract objects

--- worker.py
+++ worker.py
@@ -8,9 +8,22 @@ def readABI(file):
     with open(file, 'r') as f:
         return json.load(f)
 
+def loadContract(name, address):
+    a = readABI('abi/' + name)
+    return web3.eth.contract(abi=a, address=address)
+
+
 web3 = Web3(HTTPProvider('http://localhost:8545'))
 
-abidef = readABI('testing-contract/out/TestingContract.abi')
-mathContract = web3.eth.contract(abi=abidef, address='0xb4c79daB8f259C7Aee6E5b2Aa729821864227e84')
+mathContract = loadContract('TestingContract.abi', '0xb4c79daB8f259C7Aee6E5b2Aa729821864227e84')
 
 print (mathContract.call().Square(5))
+print (mathContract.call().Square(52))
+print (mathContract.call().Square(25))
+print (mathContract.call().Square(225))
+print (mathContract.call().Square(22225))
+print (mathContract.call().Square(345375674675))
+print (mathContract.call().Square(55555))
+print (mathContract.call().Square(5575))
+print (mathContract.call().Square(775))
+print (mathContract.call().Square(57))
